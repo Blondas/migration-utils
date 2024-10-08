e172eeeb3b0f49010571fdf613dda7d4f4b2d5a0
chris <knowakowski@protonmail.ch>
2024-10-08 08:05:58 +0200

    Delete db2_config.py

--- db2_config.py
+++
@@ -1,9 +0,0 @@
-# db2_config.py
-
-DB2_CONFIG = {
-    "database": "your_database_name",
-    "hostname": "your_hostname",
-    "port": "your_port",
-    "username": "your_username",
-    "password": "your_password"
-}
