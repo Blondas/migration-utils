3b9a3c002170664021fa151d9f8ac57eeeed1080
Krzysiek Nowakowski <knowakowski@pm.me>
2024-10-15 17:04:46 +0200

    testing async

--- logging_config.py
+++ logging_config.py
@@ -1,31 +1,21 @@
 import logging
-import os
+from typing import Tuple
 
-def setup_logging(log_file, error_log_file, include_thread_name=False):
-    log_dir = './out/logs'
-    os.makedirs(log_dir, exist_ok=True)
+def setup_logging(log_file: str, err_log_file: str) -> Tuple[logging.Logger, logging.Logger]:
+    logger = logging.getLogger()
+    error_logger = logging.getLogger()
 
-    logging_format = '%(asctime)s - %(levelname)s - '
-    if include_thread_name:
-        logging_format += '%(threadName)s - '
-    logging_format += '%(message)s'
-
-    # Configure logging to console and file
-    logging.basicConfig(
-        level=logging.INFO,
-        format=logging_format,
-        datefmt='%Y-%m-%d %H:%M:%S',
-        handlers=[
-            logging.StreamHandler(),
-            logging.FileHandler(f'{log_dir}/{log_file}')
-        ]
-    )
-
-    # Set up error logging
-    error_logger = logging.getLogger('error_logger')
+    logger.setLevel(logging.INFO)
     error_logger.setLevel(logging.ERROR)
-    error_handler = logging.FileHandler(f'{log_dir}/{error_log_file}')
-    error_handler.setFormatter(logging.Formatter(logging_format))
-    error_logger.addHandler(error_handler)
 
-    return logging.getLogger(__name__), error_logger
+    file_handler = logging.FileHandler(log_file)
+    error_file_handler = logging.FileHandler(err_log_file)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    error_file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    error_logger.addHandler(error_file_handler)
+
+    return logger, error_logger
