c4371120d8ce090cb1e0be5d675cf98ab72b4d6c
Krzysiek Nowakowski <knowakowski@pm.me>
2024-10-15 17:16:15 +0200

    paths improved

--- logging_config.py
+++ logging_config.py
@@ -1,15 +1,19 @@
 import logging
 from typing import Tuple
+import os
 
 def setup_logging(log_file: str, err_log_file: str) -> Tuple[logging.Logger, logging.Logger]:
+    log_dir = './out/logs'
+    os.makedirs(log_dir, exist_ok=True)
+
     logger = logging.getLogger()
     error_logger = logging.getLogger()
 
     logger.setLevel(logging.INFO)
     error_logger.setLevel(logging.ERROR)
 
-    file_handler = logging.FileHandler(log_file)
-    error_file_handler = logging.FileHandler(err_log_file)
+    file_handler = logging.FileHandler(f'{log_dir}/{log_file}')
+    error_file_handler = logging.FileHandler(f'{log_dir}/{err_log_file}')
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
